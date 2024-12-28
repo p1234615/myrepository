4cb0661250a91f8f00c460d2ee21f0a2692a753a
Pradip Shinde <pspatil544@gmail.com>
2024-12-28 15:52:34 +0530

    commit

---
+++ Bank4-1/src/main/java/com/example/em/Bank41Application.java
@@ -0,0 +1,13 @@
+package com.example.em;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bank41Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bank41Application.class, args);
+	}
+
+}
---
+++ Bank4-1/src/test/java/com/example/em/Bank41ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.em;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bank41ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
